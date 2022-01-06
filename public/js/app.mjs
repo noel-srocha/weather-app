29ec4585855e3e2a3aea4cd5020ac94fdef8dd5a
noel-srocha <manoelrocha.neto.93@hotmail.com>
2022-01-06 20:10:01 -0300

    Updates app.mjs

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (event) => {
 
   weatherResult.textContent = 'Loading...';
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((weatherData) => {
       if (weatherData.error) weatherResult.textContent = weatherData.error;
       else {
